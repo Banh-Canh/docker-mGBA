2790015975cf45ca519098006462af08893bf8d9
Banh-Canh <TKVictor-Hang@outlook.fr>
2021-04-06 18:07:11 +0200

    fix: added -shared to x11vnc to allow multiple connections

--- mgba.sh
+++ mgba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Xvfb :1 -screen 0 1920x1080x24 -listen tcp -ac &
-x11vnc -display :1 -nowf -noipv6 -reopen -forever -repeat -rfbport 5900 -noxdamage -bg &
+x11vnc -display :1 -nowf -noipv6 -reopen -forever -shared -repeat -rfbport 5900 -noxdamage -bg &
 fluxbox &
 websockify -D --web=/usr/share/novnc/ 6180 localhost:5900 > /dev/null 2>&1 &
 mgba-qt -f /roms/rom.gba
